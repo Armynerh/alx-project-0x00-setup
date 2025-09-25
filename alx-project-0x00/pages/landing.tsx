11e3cedd9264732fe55f900b11852c6ac68b8902
Armynerh <aimenah@outlook.com>
2025-09-25 14:49:57 -0400

    Fix button component and props

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,12 +1,11 @@
-import { Button } from "@/components/Button"
+import Button  from "@/components/Button"
 import Card from "@/components/Card"
-import Button from "@/components/Button"
 const Landing: React.FC =  () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
-      <Button  />
+      <Button size="medium" shape="rounded-md" title="Click Me" style="" />
     </div>
   )
 }
