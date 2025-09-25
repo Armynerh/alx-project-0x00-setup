678c4dd5300c2dddfdb9c45d930e9854e31bb2d2
Armynerh <aimenah@outlook.com>
2025-09-25 14:53:52 -0400

    Fix button component and props

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -3,7 +3,7 @@ export interface PillProps {
 }
 export interface ButtonProps {
   size: 'small' | 'medium' | 'large'
-  shape: 'rounded-sm' | 'rounded-md' | 'rounded-full'
+  shape: 'rounded-sm' | 'rounded-lg' | 'rounded-full'
   title: string
   style: string
 }
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,11 +1,14 @@
-import Button  from "@/components/Button"
 import Card from "@/components/Card"
+import Button  from "@/components/Button"
+
 const Landing: React.FC =  () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
-      <Button size="medium" shape="rounded-md" title="Click Me" style="" />
+      <Button size="medium" shape="rounded-sm" title="Click Me" style="" />
+      <Button size="medium" shape="rounded-lg" title="Click Me" style="" />
+      <Button size="medium" shape="rounded-full" title="Click Me" style="" />
     </div>
   )
 }
